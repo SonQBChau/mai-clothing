e0e20725e5f6fe2f39244dd5892b8a6635014941
SonChau <soncqb@gmail.com>
2020-06-01 11:38:03 +0700

    110. Card Dropdown Component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,12 +5,14 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
+
     <div className="options">
       <Link className="option" to="/shop">
         SHOP
@@ -29,6 +31,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
